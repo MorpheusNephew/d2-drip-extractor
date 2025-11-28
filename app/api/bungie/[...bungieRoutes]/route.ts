75f04ae02d6f48c5a03568e52174c1d7544c5116
Jelani Jackson <jelani.jackson@zapier.com>
2025-11-28 15:40:27 -0500

    Testing requesting session

--- app/api/bungie/[...bungieRoutes]/route.ts
+++ app/api/bungie/[...bungieRoutes]/route.ts
@@ -3,7 +3,17 @@ import { NextRequest } from "next/server";
 export async function GET(request: NextRequest) {
   console.log(request.nextUrl.pathname);
 
-  console.log({ url: `https://${request.nextUrl.hostname}/api/auth/session` });
+  const sessionUrl = `https://${request.nextUrl.hostname}/api/auth/session`;
+
+  const res = await fetch(sessionUrl, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await res.json();
+
+  console.log({ data: JSON.stringify(data) });
 
   return Response.json({ status: 200, message: "Hello" });
 }
