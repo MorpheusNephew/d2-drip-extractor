dfb23e4cc4e13cf345f7d71fd1e7fb40b1388b33
Jelani Jackson <jelani.jackson@zapier.com>
2025-11-28 16:45:16 -0500

    More printing

--- app/api/bungie/[...bungieRoutes]/route.ts
+++ app/api/bungie/[...bungieRoutes]/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: NextRequest) {
   const session = await getServerSession(authConfig);
 
   console.log({
-    validSession: !!!session?.accessToken,
+    accessToken: session?.accessToken,
     name: session?.user?.name,
   });
 
